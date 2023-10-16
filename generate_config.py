52d9e82fe1fd3c18e792adf5a7e239ae4ec4edac
Pierre Penhouet <pierre.penhouet@sekoia.io>
2023-10-16 13:50:57 +0200

    Use jinja to simplify impl

---
+++ generate_config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+# Open input config file
+with open("intakes.yaml", "r") as fyaml:
+    data = yaml.safe_load(fyaml)
+
+# Load jinja template
+template = Environment(loader=FileSystemLoader(".")).get_template("template.j2")
+
+i=1
+# Generate one file per intake
+for item in data.get("intakes", []):
+    config = template.render(item)
+    filename = f"/etc/rsyslog.d/{i}_{item['name'].lower()}.conf"
+    # Écrire le contenu généré dans le fichier
+    with open(filename, "w") as f:
+        f.write(filename)
+    i=i+1
+
